d40703e600df610bb2d804066b77649f0da4b77f
goosee5 <obiwankanoboe@gmail.com>
2023-01-06 17:21:33 -0700

    Finished draft of portal page

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 let date = new Date();
 let year = date.getFullYear();
 
-document.querySelector('h1').innerHTML = '&copy;' + year;
+document.querySelector('h1').innerHTML = `&copy; ${year} | Allison Merrick | BYU-Idaho`;
 
 let currentdate = document.lastModified;
 
