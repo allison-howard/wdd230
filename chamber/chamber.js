3ab304c278c3252eb675074a08bc1ae73c6699f7
goosee5 <obiwankanoboe@gmail.com>
2023-01-30 18:35:34 -0700

    Week 4 second edit

--- chamber/chamber.js
+++ chamber/chamber.js
@@ -7,9 +7,19 @@ hambuton.addEventListener('click', () => {
     mainnav.classList.toggle('responsive');
 })
 
-let date = new Date();
-let year = date.getFullYear();
-
 let currentdate = document.lastModified;
 
-document.querySelector('.updated').textContent = currentdate;
+document.querySelector('.updated2').textContent = currentdate;
+
+
+var d = new Date();
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var dayOfWeek = daysOfWeek[d.getDay()];
+var day = d.getDate();
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var month = months[d.getMonth()];
+var year = d.getFullYear();
+
+const date = dayOfWeek + ", " + day + " " + month + " " + year;
+
+document.querySelector('.updated').innerHTML = date;
